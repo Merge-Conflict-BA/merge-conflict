add94f9896cb1cb6888a6284eef109bf2bf7c635
Markus Haubold <Markus-Haubold@web.de>
2024-02-16 15:17:39 +0100

    stable version mainmenu, settings and levelup

--- Merge Conflict/Assets/Scripts/UiManager.cs
+++ Merge Conflict/Assets/Scripts/UiManager.cs
@@ -5,34 +5,45 @@ using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
+    //default buttons to orchestrate the menu
+    [SerializeField] private Button buttonOpenMainmenu;
+    [SerializeField] private Button buttonCloseMainmenu;
     //all buttons used in the menu
     [SerializeField] private Button buttonSettings;
     [SerializeField] private Button buttonLevel;
 
     //all menu screens
-    [SerializeField] private  Canvas screenSettings;
-    [SerializeField] private Canvas screenLevel;
+    [SerializeField] private Canvas PLAYFIELD;
+    [SerializeField] private Canvas UI_MANAGER;
+    [SerializeField] private Canvas MAINMENU;
+    [SerializeField] private Canvas SETTINGS;
+    [SerializeField] private Canvas LEVELUP;
 
+    List<KeyValuePair<string, string>> buttonRelationScreen = new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("ButtonOpenMainmenu", "Mainmenu"),
+            new KeyValuePair<string, string>("ButtonCloseMainmenu", "CloseMenu"),
+            new KeyValuePair<string, string>("ButtonOpenSettings", "Settings"),
+            new KeyValuePair<string, string>("ButtonOpenLevelup", "Levelup"),
+        };
 
-
+    Canvas currentOpenedScreen;
 
     // Start is called before the first frame update
     void Start()
     {
         //hide all submenus at startup
-        this.screenSettings.enabled = false;
-        this.screenLevel.enabled = false;
+        UI_MANAGER.enabled = true;
+        MAINMENU.enabled = false;
+        SETTINGS.enabled = false;
+        LEVELUP.enabled = false;
 
         //setup all buttons
-        setupButtonListener(this.buttonSettings);
-        setupButtonListener(this.buttonLevel);
+        setupButtonListener(buttonOpenMainmenu);
+        setupButtonListener(buttonCloseMainmenu);
+        setupButtonListener(buttonSettings);
+        setupButtonListener(buttonLevel);
        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void setupButtonListener(Button button) 
@@ -50,9 +61,52 @@ public class UiManager : MonoBehaviour
     // Diese Methode wird aufgerufen, wenn der Button geklickt wird
     private void handleButtonClick(string clickedButton)
     {
-        Debug.Log($"Button {clickedButton} wurde geklickt!");
+        screenSwitcher(clickedButton);
     }
 
+    private void screenSwitcher(string requestedScreen)
+    {
+        if(!currentOpenedScreen)
+        {
+            currentOpenedScreen = MAINMENU;
+        }
 
+        //close current opened screen
+        currentOpenedScreen.enabled = false;
+        Debug.Log("Close " + currentOpenedScreen.name);
+
+        //open requested screen
+        KeyValuePair<string, string> screenname = buttonRelationScreen.Find(pair => pair.Key == requestedScreen);
+
+        switch (screenname.Value)
+        {
+            case "Mainmenu":
+                MAINMENU.enabled = true;
+                PLAYFIELD.enabled = false;
+                currentOpenedScreen = MAINMENU;
+                break;
+
+            case "Settings":
+                SETTINGS.enabled = true;
+                currentOpenedScreen = SETTINGS;
+                break;
+                
+            case "Levelup":
+                LEVELUP.enabled = true;
+                currentOpenedScreen = LEVELUP;
+                break;
+                
+            case "CloseMenu":
+                MAINMENU.enabled = false;
+                 PLAYFIELD.enabled = true;
+                currentOpenedScreen = null;
+                break;
+
+            default:
+                Debug.Log("do somthing");
+                break;
+        }
+
+    }
 
 }
