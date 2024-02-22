3b5bdd0447e0b87bbdaaf62b2d944aeeb402c03d
Markus Haubold <Markus-Haubold@web.de>
2024-02-22 19:44:03 +0100

    add CheckForOverlap() to detect if sprites are dragged over another

--- Merge Conflict/Assets/Scripts/ComponentHandler.cs
+++ Merge Conflict/Assets/Scripts/ComponentHandler.cs
@@ -10,15 +10,18 @@ using UnityEngine;
 
 public class Dragging : MonoBehaviour
 {
+    private Debugger debug = new Debugger();
     private bool draggingActive = false;
     private Vector3 offsetMouseToCamera;
 
 
     void Update()
     {
+
         if (draggingActive)
         {
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offsetMouseToCamera;
+            CheckForOverlap();
         }
     }
 
@@ -31,6 +34,7 @@ public class Dragging : MonoBehaviour
 
     private void OnMouseUp()
     {
+
         draggingActive = false;
     }
 
@@ -67,5 +71,25 @@ public class Dragging : MonoBehaviour
         }
 
         return highestSortingOrder;
+    }
+
+    private void CheckForOverlap()
+    {
+        // Definiere den Radius des Überlappungsbereichs (kann je nach Bedarf angepasst werden)
+        float overlapRadius = 10.0f;
+
+        // Überprüfe, ob andere Collider im Überlappungsbereich sind
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, overlapRadius);
+
+        // Durchlaufe alle überlappenden Collider
+        foreach (Collider2D collider in colliders)
+        {
+            // Überprüfe, ob es sich um ein anderes Sprite handelt (kann je nach Bedarf angepasst werden)
+            if (collider.gameObject != gameObject && collider.CompareTag("component"))
+            {
+                // Handle die Überlappung hier, z.B. schreibe ein Log
+                Debug.Log("Die Sprites überlappen sich!");
+            }
+        }
     }
 }
