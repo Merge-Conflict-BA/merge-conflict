e49a018fc61c6b69b3f6c0009e873e82a74f60d1
DanielRittrich <danielrittrich@web.de>
2024-02-24 18:13:18 +0100

    Nachtrag

--- Merge Conflict/Assets/Scripts/PcPartManagerTest.cs
+++ Merge Conflict/Assets/Scripts/PcPartManagerTest.cs
@@ -13,12 +13,14 @@ public class PcPartManagerTest : MonoBehaviour
     {
         RectTransform rectTransform = GetComponent<RectTransform>();
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
 
         spriteRenderer.sprite = data.ElementSprite;
 
         rectTransform.sizeDelta = new Vector2(data.SizeWidth, data.SizeHeight);
         transform.localScale = new Vector2(data.ScaleX, data.ScaleY);
-
+        boxCollider2D.isTrigger = true;
+        boxCollider2D.size = new Vector2(data.SizeBoxColliderWidth, data.SizeBoxColliderHeight);
     }
 
     void Update()
