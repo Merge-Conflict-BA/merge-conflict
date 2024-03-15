e7972031701816264dc623f3309b082e5a5e748f
Markus Haubold <Markus-Haubold@web.de>
2024-03-15 11:14:45 +0100

    add error handling for GetComponentByName()

--- Merge Conflict/Assets/Scripts/Menu/Menu.cs
+++ Merge Conflict/Assets/Scripts/Menu/Menu.cs
@@ -1,20 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using TMPro;
 using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
     private Canvas _canvas;
-
-    // public Menu(Canvas menuCanvas)
-    // {
-    //     this._canvas = menuCanvas;
-    //     SetTitle();
-
-    // }
-
 
 
     public void InitializeMenu(Canvas menuCanvas)
@@ -23,10 +12,21 @@ public class Menu : MonoBehaviour
         SetTitle();
     }
 
+    public Canvas FindCanvasForMenu(string menuname)
+    {
+        GameObject foundCanvas = GameObject.Find(menuname);
+        return foundCanvas.GetComponent<Canvas>();
+    }
 
     public T GetComponentByName<T>(string name) where T : Component
     {
         Transform foundGameObject = _canvas.transform.Find(name);
+        if (foundGameObject == null)
+        {
+            Debugger.LogError($"The GameObject with the name {name} can't be found!");
+            return null;
+        }
+
         if (!foundGameObject.TryGetComponent<T>(out var component))
         {
             Debugger.LogError($"The component of type {typeof(T)} with the name {name} can't be found in the canvas {_canvas} !");
@@ -34,12 +34,6 @@ public class Menu : MonoBehaviour
         }
 
         return component;
-    }
-
-    public Canvas FindCanvasForMenu(string menuname)
-    {
-        GameObject foundCanvas = GameObject.Find(menuname);
-        return foundCanvas.GetComponent<Canvas>();
     }
 
     public void SetTitle()
