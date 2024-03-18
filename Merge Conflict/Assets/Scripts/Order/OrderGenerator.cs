1422f3ae28e0f90f675c946d2c1644fa10b099ff
Markus Haubold <Markus-Haubold@web.de>
2024-03-19 00:40:52 +0100

    style dataLog and add some constants

--- Merge Conflict/Assets/Scripts/Order/OrderGenerator.cs
+++ Merge Conflict/Assets/Scripts/Order/OrderGenerator.cs
@@ -7,21 +7,18 @@ public class OrderGenerator : MonoBehaviour
     private static OrderGenerator _instance;
     public static OrderGenerator Instance { get { return _instance; } }
     private ParameterStorage _parameterStorage;
-    public int currentLevel = 1;
+    private readonly string[] ComponentNames = { "RAM", "HDD", "GPU", "CPU", "Case", "Motherboard", "Powersupply" };
+    public byte currentLevel = 1;
     public bool writeLog = false;
-    const byte InitialLevel = 1;
-    const byte EndLevel = 10;
-    const byte stage1 = 0;
-    const byte stage2 = 1;
-    const byte stage3 = 2;
-    const byte stage4 = 3;
+    const byte Stage1 = 0;
+    const byte Stage2 = 1;
+    const byte Stage3 = 2;
+    const byte Stage4 = 3;
 
 
     public bool calcProbs = false;
 
-
     //define calculation parameters for component
-    //case
     public enum Case
     {
         UnlocklevelStage1 = 1,
@@ -110,8 +107,6 @@ public class OrderGenerator : MonoBehaviour
         }
         else
         {
-
-            string[] ComponentNames = { "RAM", "HDD", "GPU", "CPU", "Case", "Motherboard", "Powersupply" };
             _parameterStorage = new ParameterStorage(ComponentNames);
 
             //case
@@ -155,15 +150,8 @@ public class OrderGenerator : MonoBehaviour
         }
     }
 
-    private float[] CalculateComponentStageProbabilities(int currentLevel, string componentName)
+    private float[] CalculateComponentstageProbabilities(int currentLevel, string componentName)
     {
-        //return null if current level is not valid 
-        if (currentLevel != Math.Clamp(currentLevel, InitialLevel, EndLevel)) 
-        { 
-            Debugger.LogError("The given level in CalculateComponentStageProbabilities() is not valid! Check if the value is within the borders InitialLevel and EndLEvel.");
-            return null; 
-            };
-
         int[] distances = Calculate.DistanceCurrentLevelToEvolutionUnlockLevels(currentLevel, _parameterStorage.GetAllEvolutionUnlockLevels(componentName)); ;
 
         int[] scaledDistances = Calculate.ScaledDistance(distances, _parameterStorage.GetAllDistanceScalingFactors(componentName));
@@ -173,14 +161,15 @@ public class OrderGenerator : MonoBehaviour
         return Calculate.Probabilities(scaledDistances, totalScaledDistance);
     }
 
-    public int SelectComponentstage(string componentName, int currentLevel)
+    public byte SelectComponentstage(string componentName, int currentLevel)
     {
-        float[] stageProbabilities = CalculateComponentStageProbabilities(currentLevel, componentName);
+        byte returnedStage = 0;
+        float[] stageProbabilities = CalculateComponentstageProbabilities(currentLevel, componentName);
 
-        float p1 = stageProbabilities[stage1];
-        float p2 = stageProbabilities[stage2];
-        float p3 = stageProbabilities[stage3];
-        float p4 = stageProbabilities[stage4];
+        float p1 = stageProbabilities[Stage1];
+        float p2 = stageProbabilities[Stage2];
+        float p3 = stageProbabilities[Stage3];
+        float p4 = stageProbabilities[Stage4];
 
         float scaledP1 = p1;
         float scaledP2 = scaledP1 + p2;
@@ -189,70 +178,62 @@ public class OrderGenerator : MonoBehaviour
 
         float randomNumber = UnityEngine.Random.value;
 
-        if (randomNumber < scaledP1){return 1;}
-        if (randomNumber < scaledP2){return 2;}
-        if (randomNumber < scaledP3){return 3;}
-        else{return 4;}
-
+        if (randomNumber < scaledP1) { return returnedStage = 1; }
+        if (randomNumber < scaledP2) { return returnedStage = 2; }
+        if (randomNumber < scaledP3) { return returnedStage = 3; }
+        if (randomNumber < scaledP4) { return returnedStage = 4; }
+        return returnedStage;   //if the randomNumber fits with nothing, 0 will be return and the Component class should run into an error
     }
 
     private void WriteDataLogFile()
     {
         const string FilePath = "Assets/Scripts/Order/dataLog.txt";
-        const byte AmountLevel = 10;
-        const byte stage1 = 0;
-        const byte stage2 = 1;
-        const byte stage3 = 2;
-        const byte stage4 = 3;
+        const byte AmountLevels = 10;
 
         using (StreamWriter writer = new StreamWriter(FilePath))
         {
-
-            string[] components = { "Case", "HDD", "Powersupply", "Motherboard", "GPU", "CPU", "RAM" };
-
-
-            foreach (var component in components)
+            foreach (var component in ComponentNames)
             {
                 writer.WriteLine($"---[Component {component}]---");
                 writer.WriteLine("distanceScalingFactors:");
                 int[] dsf = _parameterStorage.GetAllDistanceScalingFactors(component);
-                writer.WriteLine("DistanceScalingFactor1: " + dsf[stage1]);
-                writer.WriteLine("DistanceScalingFactor2: " + dsf[stage2]);
-                writer.WriteLine("DistanceScalingFactor3: " + dsf[stage3]);
-                writer.WriteLine("DistanceScalingFactor4: " + dsf[stage4]);
+                writer.WriteLine("DistanceScalingFactor1: " + dsf[Stage1]);
+                writer.WriteLine("DistanceScalingFactor2: " + dsf[Stage2]);
+                writer.WriteLine("DistanceScalingFactor3: " + dsf[Stage3]);
+                writer.WriteLine("DistanceScalingFactor4: " + dsf[Stage4]);
                 writer.WriteLine(" ");
                 writer.WriteLine("unlockEvolutionLevels:");
                 int[] eul = _parameterStorage.GetAllEvolutionUnlockLevels(component);
-                writer.WriteLine("evolutionUnlockLevel1: " + eul[stage1]);
-                writer.WriteLine("evolutionUnlockLevel2: " + eul[stage2]);
-                writer.WriteLine("evolutionUnlockLevel3: " + eul[stage3]);
-                writer.WriteLine("evolutionUnlockLevel4: " + eul[stage4]);
+                writer.WriteLine("evolutionUnlockLevel1: " + eul[Stage1]);
+                writer.WriteLine("evolutionUnlockLevel2: " + eul[Stage2]);
+                writer.WriteLine("evolutionUnlockLevel3: " + eul[Stage3]);
+                writer.WriteLine("evolutionUnlockLevel4: " + eul[Stage4]);
                 writer.WriteLine(" ");
                 writer.WriteLine("Data that depends on current level:");
-                for (int level = 1; level <= AmountLevel; level++)
+                for (int level = 1; level <= AmountLevels; level++)
                 {
                     int[] dist = Calculate.DistanceCurrentLevelToEvolutionUnlockLevels(level, _parameterStorage.GetAllEvolutionUnlockLevels(component));
                     int[] scaledDist = Calculate.ScaledDistance(dist, _parameterStorage.GetAllDistanceScalingFactors(component));
                     int totalScaledDist = Calculate.TotalScaledDistances(scaledDist);
                     float[] prob = Calculate.Probabilities(scaledDist, totalScaledDist);
 
-                    writer.WriteLine($" Level {level}");
-                    writer.WriteLine($"  totalScaledDistance: " + totalScaledDist);
-                    writer.WriteLine($"  distanceToUnlockLevel1: " + dist[stage1]);
-                    writer.WriteLine($"  scaledDistance1: " + scaledDist[stage1]);
-                    writer.WriteLine($"  probability1: " + prob[stage1]);
-                    writer.WriteLine(" ");
-                    writer.WriteLine($"  distanceToUnlockLevel2: " + dist[stage2]);
-                    writer.WriteLine($"  scaledDistance2: " + scaledDist[stage2]);
-                    writer.WriteLine($"  probability2: " + prob[stage2]);
-                    writer.WriteLine(" ");
-                    writer.WriteLine($"  distanceToUnlockLevel3: " + dist[stage3]);
-                    writer.WriteLine($"  scaledDistance3: " + scaledDist[stage3]);
-                    writer.WriteLine($"  probability3: " + prob[stage3]);
-                    writer.WriteLine(" ");
-                    writer.WriteLine($"  distanceToUnlockLevel4: " + dist[stage4]);
-                    writer.WriteLine($"  scaledDistance4: " + scaledDist[stage4]);
-                    writer.WriteLine($"  probability4: " + prob[stage4]);
+                    writer.WriteLine($"|-Level {level}");
+                    writer.WriteLine($" |-totalScaledDistance: " + totalScaledDist);
+                    writer.WriteLine($" |-distanceToUnlockLevel1: " + dist[Stage1]);
+                    writer.WriteLine($" |-scaledDistance1: " + scaledDist[Stage1]);
+                    writer.WriteLine($" |-probability1: " + prob[Stage1]);
+                    writer.WriteLine(" |");
+                    writer.WriteLine($" |-distanceToUnlockLevel2: " + dist[Stage2]);
+                    writer.WriteLine($" |-scaledDistance2: " + scaledDist[Stage2]);
+                    writer.WriteLine($" |-probability2: " + prob[Stage2]);
+                    writer.WriteLine(" |");
+                    writer.WriteLine($" |-distanceToUnlockLevel3: " + dist[Stage3]);
+                    writer.WriteLine($" |-scaledDistance3: " + scaledDist[Stage3]);
+                    writer.WriteLine($" |-probability3: " + prob[Stage3]);
+                    writer.WriteLine(" |");
+                    writer.WriteLine($" |-distanceToUnlockLevel4: " + dist[Stage4]);
+                    writer.WriteLine($" |-scaledDistance4: " + scaledDist[Stage4]);
+                    writer.WriteLine($" |-probability4: " + prob[Stage4]);
                     writer.WriteLine(" ");
                 }
                 writer.WriteLine("");
