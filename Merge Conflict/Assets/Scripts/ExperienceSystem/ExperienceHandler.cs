4eaef51bf39b4b348ececc20521bbd7fe2e86e6c
Markus Haubold <Markus-Haubold@web.de>
2024-03-21 21:56:03 +0100

    add method GetXpToUnlockNextLevel() to LevelManager

--- Merge Conflict/Assets/Scripts/ExperienceSystem/ExperienceHandler.cs
+++ Merge Conflict/Assets/Scripts/ExperienceSystem/ExperienceHandler.cs
@@ -2,11 +2,13 @@ using ExperienceSystem;
 using UnityEngine;
 
 /**********************************************************************************************************************
-Name:          ExperienceHandler
-Description:   The static class ExperienceHandler provides functions for managing experience points. There is an object LevelTableSO that contains data about various game levels. Inside the "Ressources" folder in Unity, you'll find LevelData where you can set the levels, point counts, and available characters for each level.
-Author:    Anna Kozachuk
-Date:          2024-03-11
-TODO:          already discussed with Markus, how he gets the current Level
+Name:           ExperienceHandler
+Description:    The static class ExperienceHandler provides functions for managing experience points. There is an object 
+                LevelTableSO that contains data about various game levels. Inside the "Ressources" folder in Unity, you'll find LevelData where you can set the levels, point counts, and available characters for each level.
+Author:         Anna Kozachuk, Markus Haubold 
+Date:           2024-03-21
+Version:        V1.1 
+TODO:           - 
 **********************************************************************************************************************/
 
 namespace ExperienceSystem
@@ -14,7 +16,7 @@ namespace ExperienceSystem
     public static class ExperienceHandler
     {
         private static LevelTableSO _levelsData;
-        
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void InitExperienceHandler()
         {
@@ -23,7 +25,7 @@ namespace ExperienceSystem
                 _levelsData = Resources.Load<LevelTableSO>("LevelData");
             }
         }
-        
+
         public static void AddExperiencePoints(int experiencePoints)
         {
             var currentExp = PlayerPrefs.GetInt("Experience");
@@ -50,6 +52,11 @@ namespace ExperienceSystem
         public static ComponentHandler[] GetGameUnlockPrefabs()
         {
             return _levelsData.GetGameUnlockPrefabs(GetCurrentLevel());
+        }
+
+        public static int GetXpToUnlockNextLevel(int nextLevel)
+        {
+            return _levelsData.Levels[nextLevel].RequirementExperience;
         }
     }
 }
