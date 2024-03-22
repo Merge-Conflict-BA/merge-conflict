b83c4b1ea91d080c0957f084df27478adfed954b
Markus Haubold <Markus-Haubold@web.de>
2024-03-22 10:39:26 +0100

    changes from PR feedback LevelMenu and Menu

--- Merge Conflict/Assets/Scripts/Menu/LevelMenu.cs
+++ Merge Conflict/Assets/Scripts/Menu/LevelMenu.cs
@@ -8,8 +8,12 @@ public class LevelMenu : Menu
     public static LevelMenu Instance { get { return _instance; } }
     public Canvas levelmenuCanvas; //connect to the level menu canvas
     private TextMeshProUGUI _currentLevelValueTextfield;
+    const string CurrentLevelValueObjectName = "CurrentLevelValue";
     private TextMeshProUGUI _currentXpValueTextfield;
+    const string CurrentXpValueObjectName = "CurrentXpValue";
     private Image _levelProgressbar;
+    const string EmptyProgressbarObjectName = "Progressbar_empty";
+    const string FilledProgressbarObjectName = "Progressbar_filled";
 
     private void Awake()
     {
@@ -20,9 +24,9 @@ public class LevelMenu : Menu
         else
         {
             InitializeMenu(levelmenuCanvas);
-            _currentLevelValueTextfield = GetComponentByName<TextMeshProUGUI>("CurrentLevelValue");
-            _currentXpValueTextfield = GetComponentByName<TextMeshProUGUI>("CurrentXpValue");
-            _levelProgressbar = GetComponentByName<Image>("Progressbar_empty");
+            _currentLevelValueTextfield = GetComponentbyObjectName<TextMeshProUGUI>(CurrentLevelValueObjectName);
+            _currentXpValueTextfield = GetComponentbyObjectName<TextMeshProUGUI>(CurrentXpValueObjectName);
+            _levelProgressbar = GetComponentbyObjectName<Image>(EmptyProgressbarObjectName);
 
             _instance = this;
         }
@@ -50,8 +54,14 @@ public class LevelMenu : Menu
 
     public void SetProgressbarValue(int currentXp, int xpToNextLevel)
     {
-        Transform childimageTransform = _levelProgressbar.transform.Find("Progressbar_filled");
-        Image progressbar = childimageTransform.GetComponent<Image>();
+        //the progressbar is an overlay of 2 images: at the groundlayer there is an image of an 
+        //empty progressbar; on top of it, there is an image with the filled progressbar from which 
+        //we can control the visbile parts
+
+        //find the GameObject which contains the image from the filled progressbar
+        //if we have it, we can set the the value of the visible part of the image (=fillAmount)
+        Transform filledImage = _levelProgressbar.transform.Find(FilledProgressbarObjectName);
+        Image progressbar = filledImage.GetComponent<Image>();
 
         //normalized progressbar: 0xp => 0 | xpToNextLevel => 1
         float normalizedXp = Mathf.Clamp01(currentXp / (float)xpToNextLevel);
--- Merge Conflict/Assets/Scripts/Menu/Menu.cs
+++ Merge Conflict/Assets/Scripts/Menu/Menu.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     private Canvas _canvas;
+    const string TitleObjectName = "Title";
 
 
-    public void InitializeMenu(Canvas menuCanvas)
+    public void InitializeMenu(Canvas canvas)
     {
-        _canvas = menuCanvas;
+        _canvas = canvas;
         SetTitle();
     }
 
-    public Canvas FindCanvasForMenu(string menuname)
+    public Canvas FindCanvasForMenu(string name)
     {
-        GameObject foundCanvas = GameObject.Find(menuname);
+        GameObject foundCanvas = GameObject.Find(name);
         return foundCanvas.GetComponent<Canvas>();
     }
 
-    public T GetComponentByName<T>(string name) where T : Component
+    public T GetComponentbyObjectName<T>(string name) where T : Component
     {
         Transform foundGameObject = _canvas.transform.Find(name);
         if (foundGameObject == null)
@@ -39,7 +40,7 @@ public class Menu : MonoBehaviour
     public void SetTitle()
     {
         //get the TextMeshPro field from the title or return 
-        TextMeshProUGUI textfield = GetComponentByName<TextMeshProUGUI>("Title");
+        TextMeshProUGUI textfield = GetComponentbyObjectName<TextMeshProUGUI>(TitleObjectName);
         if (textfield == null) { return; };
 
         //set the title
