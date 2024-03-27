55208f5d2b7bbf33b4483a42add9c2fca01e3466
Markus Haubold <Markus-Haubold@web.de>
2024-03-27 11:19:05 +0100

    change comment

--- Merge Conflict/Assets/Scripts/ExperienceSystem/ExperienceHandler.cs
+++ Merge Conflict/Assets/Scripts/ExperienceSystem/ExperienceHandler.cs
@@ -5,7 +5,7 @@ Name:           ExperienceHandler
 Description:    The static class ExperienceHandler provides functions for managing experience points. There is an object 
                 LevelTableSO that contains data about various game levels. Inside the "Ressources" folder in Unity, you'll find LevelData where you can set the levels, point counts, and available characters for each level.
 Author:         Anna Kozachuk, Markus Haubold 
-Date:           2024-03-21
+Date:           2024-03-26
 Version:        V1.1 
 TODO:           - 
 **********************************************************************************************************************/
@@ -55,9 +55,11 @@ namespace ExperienceSystem
 
         public static int NeededXpToUnlockNextLevel(int currentLevel)
         {
-            //the LevelData array counts from 0...9; so we can use the 
-            //current level to get the xp from the upcoming level: current 
-            //level=2 => LevelData[2] means value from level 3
+            /*
+             * the LevelData array counts from 0...9; so we can use the 
+             * current level to get the xp from the upcoming level: current 
+             * level=2 => LevelData[2] means value from level 3
+            */
             return _levelsData.Levels[currentLevel].RequirementExperience;
         }
     }
